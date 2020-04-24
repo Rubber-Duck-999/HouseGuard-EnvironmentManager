dc7658c8601485fdb741bb09cb00781c3497075d
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2020-04-24 09:45:29 +0000

    Update to EVM to communicate with google drive api, work in progress

--- src/exeEnvironmentManager.go
+++ src/exeEnvironmentManager.go
@@ -1,30 +1,32 @@
 package main
 
 import (
-	"os"
+	//"os"
 
-	"github.com/akamensky/argparse"
+	//"github.com/akamensky/argparse"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	log.SetLevel(log.DebugLevel)
 	log.Warn("EVM - Beginning to run Environment Manager Program")
-	parser := argparse.NewParser("file", "Config file for runtime purpose")
+	//parser := argparse.NewParser("file", "Config file for runtime purpose")
 	// Create string flag
-	f := parser.String("f", "config", &argparse.Options{Required: true, Help: "Necessary config"})
+	//f := parser.String("f", "config", &argparse.Options{Required: true, Help: "Necessary config"})
 	// Parse input
-	err := parser.Parse(os.Args)
+	/*err := parser.Parse(os.Args)
 	if err != nil {
 		log.Error("Arguments passed incorrectly")
 		// In case of error print error and print usage
 		// This can also be done by passing -h or --help flags
 		log.Error(parser.Usage(err))
 		os.Exit(2)
-	}
+	}*/
 
-	file := *f
-	var data ConfigTypes
+	//file := *f
+	//var data ConfigTypes
+	google_drive()
+	/*
 	if Exists(file) {
 		GetData(&data, file)
 	} else {
@@ -34,5 +36,5 @@ func main() {
 	log.Trace(data)
 	SetPassword(data.Settings.Pass)
 	SetKeys(data.Settings.Key)
-	Subscribe()
+	Subscribe()*/
 }
